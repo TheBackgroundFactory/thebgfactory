cebe697ae92b9ab346d4b8fa46831440558b8911
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2015-10-10 15:31:41 -0400

    Fixed Clear Bug

--- graphics.js
+++ graphics.js
@@ -13,28 +13,41 @@ var nextY = -1
 var mouseDown = 0
 
 var offset = 15
-var sqrDim = 10
-
-var h = 0
 
 function init(){
-    //ctx.fillStyle = 'rgb(255,255,255)'
+    ctx.fillStyle = 'rgb(255,255,255)'
     ctx.fillRect (0, 0, w, h);	
-  	draw();
 }
 
-window.addEventListener('mousemove', function(e){
-  nextX = e.clientX;
-  nextY = e.clientY;
-  draw()
-});
+function fixPageXY(e)
+      {
+        if (e.pageX == null && e.clientX != null ) 
+        { 
+            var html = document.documentElement
+            var body = document.body
 
-document.body.onmousedown=function(){mouseDown = 1;}
-document.body.onmouseup=function(){mouseDown = 0;}
+            e.pageX = e.clientX + (html.scrollLeft || body && body.scrollLeft || 0)
+            e.pageX -= html.clientLeft || 0
+    
+            e.pageY = e.clientY + (html.scrollTop || body && body.scrollTop || 0)
+            e.pageY -= html.clientTop || 0
+          }
+      }
+      document.onmousemove = function(e) 
+      {
+          e = e || window.event
+          fixPageXY(e)
+          nextX = e.pageX;
+          nextY = e.pageY;
+          draw();
+      }
 
-document.body.onkeydown=function(){ctx.clearRect(0,0,w,h)};
+window.onmousedown=function(){mouseDown = 1;}
+window.onmouseup=function(){mouseDown = 0;}
+window.onkeydown=function(){ctx.clearRect(0,0,w,h);};
 
 function draw() {
+  //ctx.save();
   ctx.fillStyle = 'rgba(10,10,10,0.5)'
   //ctx.fillRect (offset, offset, w-offset*2, h-offset*2);
 
@@ -48,26 +61,29 @@ function draw() {
   //h = h + 1;
   //ctx.strokeStyle = 'rgb('+rgb[0]+','+rgb[1]+','+rgb[2]+')'
   ctx.strokeStyle = 'rgba('+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+',1)'
-  
-  //ctx.fillRect (x-sqrDim/2, y-sqrDim/2, sqrDim, sqrDim);
 
-  if (mouseDown == 1){
-    drawLine(lastX, lastY, nextX, nextY)
-    drawLine(w-lastX, lastY, w-nextX, nextY)
+  var dx = Math.abs(nextX-lastX)
+  var dy = Math.abs(nextY-lastY)
+
+  if ((dx+dy)/2 > 6){
+    if (mouseDown == 1){
+      drawLine(lastX, lastY, nextX, nextY)
+      drawLine(w-lastX, lastY, w-nextX, nextY)
+    }
+
+    lastX = nextX
+    lastY = nextY
   }
-
-  lastX = nextX
-  lastY = nextY
+  //ctx.restore();
 }
 
 function drawLine(x0, y0, x1, y1){
-  var t = new Date();
-
   ctx.beginPath();
   ctx.moveTo(x0, y0);
   ctx.lineTo(x1, y1);
   ctx.lineWidth = 8;
   ctx.stroke();
+  ctx.closePath();
 }
 
 init()
