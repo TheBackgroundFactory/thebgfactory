b7aebec6f890c5c0ac5e0244335dd404838616f1
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2015-10-10 20:55:13 -0400

    Added Interactivity to Dot Wave
    Hover lighting for background

--- dotWaveGraphics.js
+++ dotWaveGraphics.js
@@ -6,21 +6,31 @@ var w = canvas.width = window.innerWidth
 var h = canvas.height = window.innerHeight
 
 // Variable parameters
-var dotDistance = 140 // Pixels between dots 
-var pathRadius = 110 // Dot path radius
-var dotRadius = 20 // Radius of each dot
+var dotDistance = 160 // Pixels between dots 140
+var pathRadius = 125 // Dot path radius
+var dotRadius = 18 // Radius of each dot
 var dotPeriod = 3 // Period of each circle revolution
-var bShade = 50 // Shade of background
-var pathShade = 200 // Shade of circle path
+var waveFactor = 10 // Waviness of dot motion
+
+var bShade = 30 // Shade of background
 var dotShade = 255 // Shade of dot
-var dotAlpha = 0.9 // Alpha value of dot
+var dotAlpha = 0.8 // Alpha value of dot
+
 var shadColour = '#909090' // Colour of dot shadow
 var shadBlur = 10 // Shadow blur value
 var shadOffsetX = 0 // Shadow offset along x-dimension
 var shadOffsetY = 0 // Shadow offset along y-dimension
-var waveFactor = 20 // Waviness of dot motion
+
+var pathShadeRadii = [pathRadius*1.4, pathRadius*2.5, pathRadius*3.5] // Path ring cursor hover ranges
+
+var pathShades = [110, 80, 60] // Path ring cursor hover shades
+var pathShade = 45 // Default path shade
 
 var dots = []
+
+var mouseX = -1
+var mouseY = -1
+var ringDistance = -1
 
 function init() {
     for (i = 0 ; i < w/dotDistance+1 ; i ++){
@@ -50,13 +60,13 @@ function fixPageXY(e) {
 document.onmousemove = function(e) {
   e = e || window.event
   fixPageXY(e)
-  nextX = e.pageX;
-  nextY = e.pageY;
+  mouseX = e.pageX;
+  mouseY = e.pageY;
 }
 
-window.onmousedown=function(){ mouseDown = 1; }
-window.onmouseup=function(){ mouseDown = 0; }
-window.onkeydown=function(){ ctx.clearRect(0,0,w,h); };
+function getDistance(x0, y0, x1, y1){
+  return Math.sqrt(Math.pow(x1-x0, 2)+Math.pow(y1-y0, 2))
+}
 
 function draw() {
   var t = (new Date()).getMilliseconds()+1000*(((new Date()).getSeconds())%dotPeriod)
@@ -64,10 +74,20 @@ function draw() {
   ctx.fillStyle = 'rgb('+bShade+','+bShade+','+bShade+')'
   ctx.fillRect(0,0,w,h);
 
-  ctx.strokeStyle = 'rgb('+pathShade+','+pathShade+','+pathShade+')'  
-
   for (i = 0 ; i < w/dotDistance+1 ; i++){
       for (j = -1 ; j < h/dotDistance+1 ; j++){
+        var ringDistance = getDistance(i*dotDistance,j*dotDistance, mouseX, mouseY)
+
+        if (ringDistance < pathShadeRadii[0]){
+          ctx.strokeStyle = 'rgb('+pathShades[0]+','+pathShades[0]+','+pathShades[0]+')' 
+        } else if (ringDistance < pathShadeRadii[1]){
+          ctx.strokeStyle = 'rgb('+pathShades[1]+','+pathShades[1]+','+pathShades[1]+')' 
+        } else if (ringDistance < pathShadeRadii[2]){
+          ctx.strokeStyle = 'rgb('+pathShades[2]+','+pathShades[2]+','+pathShades[2]+')' 
+        } else {
+          ctx.strokeStyle = 'rgb('+pathShade+','+pathShade+','+pathShade+')' 
+        }
+
         ctx.beginPath();
         ctx.arc(i*dotDistance,j*dotDistance,pathRadius,0,2*Math.PI);
         ctx.lineWidth = 1
@@ -79,7 +99,9 @@ function draw() {
 
   for (i = 0 ; i < w/dotDistance+1 ; i++){
       for (j = -1 ; j < h/dotDistance+1 ; j++){
-        dots[i][j] = [i*dotDistance+pathRadius*Math.cos(Math.PI*2*t/(1000*dotPeriod)), j*dotDistance+pathRadius*Math.sin(Math.PI*2*t/(1000*dotPeriod))];
+        var theta = Math.PI*2*t/(1000*dotPeriod)
+
+        dots[i][j] = [i*dotDistance+pathRadius*Math.cos(theta), j*dotDistance+pathRadius*Math.sin(theta)];
 
         ctx.save()
         ctx.shadowColor = shadColour;
